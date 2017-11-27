f68a43216dd0a0b25d53baa329a653c390f5e99a
patrickshampine <patrick@patrickshampine.com>
2017-11-26 21:25:16 -0800

    Descriptions

--- vagrant/package.sh
+++ vagrant/package.sh
@@ -1,5 +1,8 @@
-vagrant package --output newbox.box
-vagrant box add newboxtoadd newbox.box
-vagrant destroy
-rm Vagrantfile
-vagrant init newboxtoadd
+## Creates a provider file
+vagrant package --output [/path/to/name.box]
+
+## Adds a provider file to local vagrant list
+vagrant box add [box_name] [/path/to/name.box]
+
+## Install new provider
+vagrant init [box_name]
